aad761a38409036663523691a910cd5ef533dd19
Chpatel7085 <126132633+Chpatel7085@users.noreply.github.com>
2023-03-13 14:50:10 +0530

    Update Player.cpp

--- Box2D with SFML/Box2D with SFML/Player.cpp
+++ Box2D with SFML/Box2D with SFML/Player.cpp
@@ -8,7 +8,9 @@
 using namespace std;
 using namespace sf;
 
+//For updating the score of the player 
 
+//Constructor
 Player::Player() {
     font.loadFromFile("yess.ttf");
     scoreText.setFont(font);
@@ -17,18 +19,23 @@ Player::Player() {
     score = 0;
 }
 
+//Getting the curr score
 int Player::getScore() {
     return score;
 }
 
+//Increase the score by 1 when brick is destroyed by the ball
 void Player::increaseScore() {
     score++;
 }
 
+//updating the score
 void Player::update(RenderWindow& window) {
     scoreText.setString("Score: " + std::to_string(score));
     scoreText.setPosition(window.getSize().x - scoreText.getLocalBounds().width - 10.0f, 10.0f);
 }
+
+//Drawing
 void Player::draw(RenderWindow& window) {
     window.draw(scoreText);
 }
